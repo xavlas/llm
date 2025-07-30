540f96febcd7d1a5e3d7ff7d93b7ad72478c83ad
xavlas <lassus.xavier@gmail.com>
2025-07-30 11:18:31 +0200

    Create 6_news.py

---
+++ pages/6_news.py
@@ -0,0 +1,24 @@
+from newsapi import NewsApiClient
+
+# Init
+newsapi = NewsApiClient(api_key='b41fd15e23ee45b2b306f39426394c1e')
+
+# /v2/top-headlines
+top_headlines = newsapi.get_top_headlines(q='bitcoin',
+                                          sources='bbc-news,the-verge',
+                                          category='business',
+                                          language='en',
+                                          country='us')
+
+# /v2/everything
+all_articles = newsapi.get_everything(q='bitcoin',
+                                      sources='bbc-news,the-verge',
+                                      domains='bbc.co.uk,techcrunch.com',
+                                      from_param='2025-07-01',
+                                      to='2025-07-12',
+                                      language='en',
+                                      sort_by='relevancy',
+                                      page=2)
+
+# /v2/top-headlines/sources
+sources = newsapi.get_sources()
